00748e89527c2d8f928b6513c522d02dbbae8831
sajjad-021 <kavir.tanha200850@gmail.com>
2016-12-20 07:09:47 -0800

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = "294803517:AAH_TBYVjGwG_PhIxo0IvPyi6_8ECU9wnRs"
+local bot_api_key = "327493127:AAHS1TMn1FJmbRhlNSnAbzKKkxfB4bBzCBY"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[Hi,
@@ -91,7 +91,7 @@ local link = [[ For Hyperlink Your Text Please Join This [Channel](https://teleg
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {323382252,294803517}-- put your id here
+  local admins = {323382252,327493127}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
